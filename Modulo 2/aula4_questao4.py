d06921d62ab43ca2c98c2b319150a3b9b5e17de5
Alexandre Ferreira <164171185+Alexandre2205@users.noreply.github.com>
2025-07-17 15:15:39 -0300

    Update aula4_questao4.py

--- Modulo 2/aula4_questao4.py
+++ Modulo 2/aula4_questao4.py
@@ -1,17 +1,12 @@
-valor= int(input('Digite o valor a ser pago: '))
-notas_100= int(valor/100)
-valor=valor%100
-notas_50=int(valor/50)
-valor=valor%50
-notas_20=int(valor/20)
-valor=valor%20
-notas_10=int(valor/10)
-valor=valor%10
-notas_5=int(valor/5)
-valor=valor%5
-notas_2= int(valor/2)
-valor= valor%2
-notas_1= int(valor/1)
+valor = int(input('Digite o valor :'))
+
+notas_100, valor = valor // 100 , valor % 100
+notas_50, valor = valor // 50 , valor % 50
+notas_20, valor = valor // 20 , valor % 20
+notas_10, valor = valor // 10 , valor % 10
+notas_5, valor = valor // 5 , valor % 5
+notas_2, valor = valor // 2 , valor % 2
+notas_1, valor = valor // 1 , valor % 1
 print(notas_100,' nota(s) de R$100,00')
 print(notas_50,' nota(s) de R$50,00')
 print(notas_20,' nota(s) de R$20,00')
